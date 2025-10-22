7f49bf4bdc6e6a6c6ea293de6e8f182ad34b6f73
Jorge Orenos <orenosjorge@gmail.com>
2025-10-22 09:04:34 -0600

    feat: The ResampleExtendedSVM now works for a ContryStructure and VarCPIBase

--- src/resample/ResampleExtendedSVM.jl
+++ src/resample/ResampleExtendedSVM.jl
@@ -4,56 +4,71 @@
 # Definition of the resampling function by month of occurrence
 """
     ResampleExtendedSVM <: ResampleFunction
-♦
-Defines a resampling function to resample the time series by the
-same months of occurrence. The sampling is performed independently for each
-time series in the columns of a matrix. 
 
-    Exaplme
+Define a resampling function to resample the time series for the same months of occurrence. 
+Sampling is performed independently for each time series in the columns of a matrix. 
+
+    Example
 
     resamplefn = ResampleExtendedSVM([150, 180, 50])
-    resamplefn(GTDATA23)
+    resamplefn (GTDATA23)
 
-    The GTDATA23 contains trhee VarCPIBase, the same length as the vector 
-    used as a parameter in ResampleExtendedSVM()
+GTDATA23 contains three VarCPIBase, of the same length as the vector used as a parameter 
+in ResampleExtendedSVM().
 """
-struct ResampleExtendedSVM <: ResampleFunction 
-    extended::Vector{Int}
+struct ResampleExtendedSVM <: ResampleFunction
+    extension_periods::Union{Int,Vector{Int}}
 end
 
 # Define how to resample matrices with time series in the columns.
 # Uses the internal function `scramblevar`.
-function (resamplefn::ResampleExtendedSVM)(cs::CountryStructure, rng = Random.GLOBAL_RNG)
+function (resamplefn::ResampleExtendedSVM)(cs::CountryStructure, rng=Random.GLOBAL_RNG)
+
+    if isa(resamplefn.extension_periods, Vector) &&
+       length(resamplefn.extension_periods) != length(cs.base)
+        error("The vector of periods must have the same number of VarCPIBase in the CountyStructure")
+    end
+
+    if length(resamplefn.extension_periods) == 1
+        periods = fill(resamplefn.extension_periods, length(cs.base))
+        println(string(resamplefn.extension_periods, "is the number of periods for all VarCPIBase"))
+    else
+        periods = resamplefn.extension_periods
+    end
 
     # Obtain resampled bases
-    ps = (resamplefn.extended...,)
-    base_boot = map((b, extended) -> resamplefn(b, extended, rng), cs.base, ps)
+    ps = (periods...,)
+    base_boot = map((b, extension_periods) -> resamplefn(b, extension_periods, rng), cs.base, ps)
     # Return new CountryStructure
     typeof(cs)(base_boot)
+
 end
 
-function (resamplefn::ResampleExtendedSVM)(base::VarCPIBase, extended, rng = Random.GLOBAL_RNG)
-    v_boot = resamplefn(base.v, extended, rng)
+function (resamplefn::ResampleExtendedSVM)(base::VarCPIBase, extension_periods::Int, rng=Random.GLOBAL_RNG)
+    
+    v_boot = resamplefn(base.v, extension_periods, rng)
     # Extend the dates
     startdate = base.dates[1]
-    dates = startdate:Month(1):Date(startdate + Month(extended - 1))
-    VarCPIBase(v_boot, base.w, dates, base.baseindex)
+    dates = startdate:Month(1):Date(startdate + Month(extension_periods - 1))
+
+    return (VarCPIBase(v_boot, base.w, dates, base.baseindex))
 end
 
-function (resamplefn::ResampleExtendedSVM)(vmat::AbstractMatrix, extended, rng = Random.GLOBAL_RNG) 
+
+function (resamplefn::ResampleExtendedSVM)(vmat::AbstractMatrix, extension_periods::Int, rng=Random.GLOBAL_RNG)
     # Number of periods and basic expenditures of the input matrix
     periods_vmat, n = size(vmat)
-    
+
     # Matrix of resampled values
-    v_sc = Matrix{eltype(vmat)}(undef, extended, n)
+    v_sc = Matrix{eltype(vmat)}(undef, extension_periods, n)
 
     # For each month and each basic expenditure, randomly take from the same
     # months of vmat and fill v_sc (v_scrambled)
-    for i in 1:min(extended, 12), g in 1:n 
-        Random.rand!(rng, view(v_sc, i:12:extended, g), view(vmat, i:12:periods_vmat, g))        
-    end    
+    for i in 1:min(extension_periods, 12), g in 1:n
+        Random.rand!(rng, view(v_sc, i:12:extension_periods, g), view(vmat, i:12:periods_vmat, g))
+    end
     v_sc
-end 
+end
 
 # Define the name and tag of the resampling method 
 method_name(resamplefn::ResampleExtendedSVM) = "Extended IID bootstrap by months of occurrence"
